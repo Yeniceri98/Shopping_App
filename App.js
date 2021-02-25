04ee64afd73642b976b7caf866ba3140f4fd3bd4
Ahmet Sami Yeniçeri <ahmetsamiyeniceri@gmail.com>
2021-02-25 14:19:50 +0300

    Commit 1

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
@@ -11,11 +11,16 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+// _____ İNDİRİLENLER _____
+// React Navigation v5  ----->  yarn add @react-navigation/native
+// Navigation Gesture Handling  ----->  expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+// Stack Navigator ----->  yarn add @react-navigation/stack
+// Tab Navigator  ----->  yarn add @react-navigation/bottom-tabs
+// Drawer Navigator  ----->  yarn add @react-navigation/drawer
+// Header Buttons  ----->  yarn add react-navigation-header-buttons  ----->  import { HeaderButton } from 'react-navigation-header-buttons';
+// Redux  ----->  yarn add redux
+// React Redux  ----->  yarn add react-redux
+// Redux Dev Tool Extension  ----->  yarn add redux-devtools-extension  ----->  import { composeWithDevTools } from 'redux-devtools-extension';
+// Custom Font  ----->  expo install expo-font
+// AppLoading  ----->  expo install expo-app-loading
